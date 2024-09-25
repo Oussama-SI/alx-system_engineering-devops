f4f06a34f05758345e5f4e11f22257853a85fdb3
OUSSAMA BOURROUS <oussama18bourrous@gmail.com>
2024-09-25 16:17:10 -0700

    donetask 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""Module that represents a function to return top 10 posts"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Function that prints the first 10 hot posts listed for
-    a given subreddit
-    subreddit: the given subreddit
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Mozzila/5.0'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
-    except resuests.ResuestException as e:
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
