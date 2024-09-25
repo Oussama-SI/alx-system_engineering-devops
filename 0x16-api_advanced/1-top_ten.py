5a98d986b2ee7e0e50168d1e03eca878b134d43a
OUSSAMA BOURROUS <oussama18bourrous@gmail.com>
2024-09-25 16:31:53 -0700

    donetask 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""Module that represents a function to return top 10 posts"""
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """
-    Function that prints the first 10 hot posts listed for
-    a given subreddit
-    subreddit: the given subreddit
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Mozzila/5.0'}
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
-    except resuests.ResuestException as e:
-        print(None)
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
